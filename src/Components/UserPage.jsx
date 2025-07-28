410aa9ad98a84618082934fde24e3d56f521db5b
Yogesh Rahav <yogeshrahav121@gmail.com>
2025-07-28 22:41:07 +0530

    training how to make  the api calls

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UserPage = () => {
     let x = 20;
     let y = 30;
     const z = x + y;
     console.log(z)
+
+    const userList = useState();
+    const [ user, setUser] = useState([]);
+    // const [ loading, setLoading] = useState(true);
+
+    useEffect(() =>{
+         fetch('https://jsonplaceholder.typicode.com/users')
+         .then((res) => res.json())
+         .then((data) =>{
+            setUser(data);
+            // setLoading(false)
+         })
+    })
   return (
     <div>
         <h2>
             Welcome {z}
         </h2>
+        <ul>
+            { user.map((user) =>(
+             <li key={user.id}>{user.id} {user.name} ({user.email})</li>
+            ))}
+        </ul>
     </div>
   )
 }
