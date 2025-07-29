134351829e19691c385cae8ee47ac5458edc7b5c
Yogesh Rahav <yogeshrahav121@gmail.com>
2025-07-29 20:27:01 +0530

    trained with the conditional rendering basic concepts.

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -6,9 +6,14 @@ const UserPage = () => {
     const z = x + y;
     console.log(z)
 
-    const userList = useState();
     const [ user, setUser] = useState([]);
+    const [ show, setShow] = useState("VIEW")
     // const [ loading, setLoading] = useState(true);
+
+    const toShow = () =>{
+        setShow((prevShow) => (prevShow === "VIEW"? "HIDE":"VIEW"))
+    }
+    
 
     useEffect(() =>{
          fetch('https://jsonplaceholder.typicode.com/users')
@@ -23,9 +28,15 @@ const UserPage = () => {
         <h2>
             Welcome {z}
         </h2>
-        <ul>
+        {/* <ul>
             { user.map((user) =>(
              <li key={user.id}>{user.id} {user.name} ({user.email})</li>
+            ))}
+        </ul> */}
+        <button onClick={toShow}>{show === "VIEW"?"HIDE":"VIEW"}</button>
+        <ul>
+            {user.map((user) =>(
+                <li key={user.id}>{user.id} {user.name} {user.email}</li>
             ))}
         </ul>
     </div>
